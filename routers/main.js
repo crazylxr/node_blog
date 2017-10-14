7e805780bf579004d13591d478fb7df7c88f30ef
lxr <1076629390@qq.com>
2017-10-14 21:54:05 +0800

    标签

--- routers/main.js
+++ routers/main.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Category = require('../models/Category');
+var Tag = require('../models/Tag');
 var Content = require('../models/Contents');
 var marked = require('marked');
 
@@ -11,17 +12,22 @@ router.use(function (req, res, next) {
     data = {
         userInfo: req.userInfo,
         categries: [],
+        tags: [],
     }
 
     Category.find().then(function (categries) {
       data.categries = categries;
-      next();
+      Tag.find().then(function (tags) {
+          data.tags = tags;
+          next();
+      })
     })
 });
 
 router.get('/', function (req, res, next) {
     Object.assign(data,{
          category: req.query.category || '',
+         tag: req.query.tag || '',
          page : Number(req.query.page || 1),
          limit : 4,
          pages : 0,
@@ -33,6 +39,10 @@ router.get('/', function (req, res, next) {
         where.category = data.category;
     }
 
+    if(data.tag){
+        where.tag = data.tag;
+    }
+
     Content.where(where).count().then(function (count) {
         data.count = count;
         data.pages = Math.ceil(data.count / data.limit);
@@ -41,7 +51,6 @@ router.get('/', function (req, res, next) {
 
         var limit = data.limit;
         var skip = (data.page - 1) * data.limit;
-
 
         return Content.where(where).find().limit(limit).skip(skip).populate(['category', 'user']).sort({
             addTime: -1
